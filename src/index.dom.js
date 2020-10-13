c35694400ff4650638e4edfdd62e8237b228d4db
Luke Edwards <luke.edwards05@gmail.com>
2020-10-13 14:32:33 -0700

    fix: include `hydrate` in config & boot

--- src/index.dom.js
+++ src/index.dom.js
@@ -2,6 +2,7 @@ import App from './App.svelte';
 
 export default new App({
 	target: document.body,
+	hydrate: true,
 	props: {
 		name: 'world'
 	}
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // Config file for Svelte – detected by VSCode & varying tools
 // @see https://github.com/sveltejs/language-tools/blob/master/docs/preprocessors/in-general.md
 module.exports = {
+	hydratable: true,
 	extensions: ['.svelte']
 }
